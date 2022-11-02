e351265afbaa092a11f4117230ba5bb7e5db7ac4
daddybilly19 <116768066+daddybilly19@users.noreply.github.com>
2022-11-02 13:40:16 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,113 @@
+def on_received_number(receivedNumber):
+    global sprite
+    if receivedNumber == 1:
+        Enemy.change(LedSpriteProperty.X, -1)
+    if receivedNumber == 2:
+        Enemy.change(LedSpriteProperty.X, 1)
+    if receivedNumber == 5:
+        Enemy.change(LedSpriteProperty.Y, -1)
+    if receivedNumber == 6:
+        Enemy.change(LedSpriteProperty.Y, 1)
+    if receivedNumber == 0:
+        sprite = game.create_sprite(Enemy.get(LedSpriteProperty.X),
+            Enemy.get(LedSpriteProperty.Y))
+        sprite.set(LedSpriteProperty.BRIGHTNESS, 100)
+        for index in range(4):
+            sprite.change(LedSpriteProperty.X, -1)
+            basic.pause(100)
+            if sprite.is_touching(Player):
+                game.remove_life(1)
+            if sprite.get(LedSpriteProperty.X) <= 0:
+                sprite.delete()
+    if receivedNumber == 3:
+        if game.is_paused():
+            game.resume()
+        else:
+            game.pause()
+    if receivedNumber == 4:
+        sprite = game.create_sprite(Enemy.get(LedSpriteProperty.X),
+            Enemy.get(LedSpriteProperty.Y))
+        sprite.set(LedSpriteProperty.BRIGHTNESS, 100)
+        for index2 in range(4):
+            sprite.change(LedSpriteProperty.Y, -1)
+            basic.pause(100)
+            if sprite.is_touching(Player):
+                game.remove_life(1)
+            if sprite.get(LedSpriteProperty.Y) <= 0:
+                sprite.delete()
+radio.on_received_number(on_received_number)
+
+def my_function():
+    global Shoot
+    radio.send_number(4)
+    Shoot = game.create_sprite(Player.get(LedSpriteProperty.X),
+        Player.get(LedSpriteProperty.Y))
+    Shoot.set(LedSpriteProperty.BRIGHTNESS, 100)
+    for index3 in range(4):
+        Shoot.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+        if Shoot.is_touching(Enemy):
+            game.add_score(1)
+        if Shoot.get(LedSpriteProperty.Y) <= 0:
+            Shoot.delete()
+    basic.pause(1000)
+    if game.score() == 4:
+        game.game_over()
+joystickbit.on_button_event(joystickbit.JoystickBitPin.P13,
+    joystickbit.ButtonType.DOWN,
+    my_function)
+
+def my_function2():
+    global Shoot
+    radio.send_number(0)
+    Shoot = game.create_sprite(Player.get(LedSpriteProperty.X),
+        Player.get(LedSpriteProperty.Y))
+    Shoot.set(LedSpriteProperty.BRIGHTNESS, 100)
+    for index4 in range(4):
+        Shoot.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+        if Shoot.is_touching(Enemy):
+            game.add_score(1)
+        if Shoot.get(LedSpriteProperty.X) <= 0:
+            Shoot.delete()
+    basic.pause(1000)
+    if game.score() == 4:
+        game.game_over()
+joystickbit.on_button_event(joystickbit.JoystickBitPin.P12,
+    joystickbit.ButtonType.DOWN,
+    my_function2)
+
+def on_logo_pressed():
+    radio.send_number(3)
+    if game.is_paused():
+        game.resume()
+    else:
+        game.pause()
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+Shoot: game.LedSprite = None
+sprite: game.LedSprite = None
+Player: game.LedSprite = None
+Enemy: game.LedSprite = None
+game.set_life(4)
+game.set_score(0)
+joystickbit.init_joystick_bit()
+Enemy = game.create_sprite(0, 0)
+Player = game.create_sprite(4, 4)
+radio.set_group(1)
+
+def on_forever():
+    if joystickbit.get_rocker_value(joystickbit.rockerType.X) <= 200:
+        radio.send_number(1)
+        Player.change(LedSpriteProperty.X, 1)
+    if joystickbit.get_rocker_value(joystickbit.rockerType.X) >= 800:
+        radio.send_number(2)
+        Player.change(LedSpriteProperty.X, -1)
+    if joystickbit.get_rocker_value(joystickbit.rockerType.Y) <= 200:
+        radio.send_number(5)
+        Player.change(LedSpriteProperty.Y, 1)
+    if joystickbit.get_rocker_value(joystickbit.rockerType.Y) >= 800:
+        radio.send_number(6)
+        Player.change(LedSpriteProperty.Y, -1)
+    basic.pause(100)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,124 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        Enemy.change(LedSpriteProperty.X, -1)
+    }
+    if (receivedNumber == 2) {
+        Enemy.change(LedSpriteProperty.X, 1)
+    }
+    if (receivedNumber == 5) {
+        Enemy.change(LedSpriteProperty.Y, -1)
+    }
+    if (receivedNumber == 6) {
+        Enemy.change(LedSpriteProperty.Y, 1)
+    }
+    if (receivedNumber == 0) {
+        sprite = game.createSprite(Enemy.get(LedSpriteProperty.X), Enemy.get(LedSpriteProperty.Y))
+        sprite.set(LedSpriteProperty.Brightness, 100)
+        for (let index = 0; index < 4; index++) {
+            sprite.change(LedSpriteProperty.X, -1)
+            basic.pause(100)
+            if (sprite.isTouching(Player)) {
+                game.removeLife(1)
+            }
+            if (sprite.get(LedSpriteProperty.X) <= 0) {
+                sprite.delete()
+            }
+        }
+    }
+    if (receivedNumber == 3) {
+        if (game.isPaused()) {
+            game.resume()
+        } else {
+            game.pause()
+        }
+    }
+    if (receivedNumber == 4) {
+        sprite = game.createSprite(Enemy.get(LedSpriteProperty.X), Enemy.get(LedSpriteProperty.Y))
+        sprite.set(LedSpriteProperty.Brightness, 100)
+        for (let index = 0; index < 4; index++) {
+            sprite.change(LedSpriteProperty.Y, -1)
+            basic.pause(100)
+            if (sprite.isTouching(Player)) {
+                game.removeLife(1)
+            }
+            if (sprite.get(LedSpriteProperty.Y) <= 0) {
+                sprite.delete()
+            }
+        }
+    }
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    radio.sendNumber(4)
+    Shoot = game.createSprite(Player.get(LedSpriteProperty.X), Player.get(LedSpriteProperty.Y))
+    Shoot.set(LedSpriteProperty.Brightness, 100)
+    for (let index = 0; index < 4; index++) {
+        Shoot.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+        if (Shoot.isTouching(Enemy)) {
+            game.addScore(1)
+        }
+        if (Shoot.get(LedSpriteProperty.Y) <= 0) {
+            Shoot.delete()
+        }
+    }
+    basic.pause(1000)
+    if (game.score() == 4) {
+        game.gameOver()
+    }
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    radio.sendNumber(0)
+    Shoot = game.createSprite(Player.get(LedSpriteProperty.X), Player.get(LedSpriteProperty.Y))
+    Shoot.set(LedSpriteProperty.Brightness, 100)
+    for (let index = 0; index < 4; index++) {
+        Shoot.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+        if (Shoot.isTouching(Enemy)) {
+            game.addScore(1)
+        }
+        if (Shoot.get(LedSpriteProperty.X) <= 0) {
+            Shoot.delete()
+        }
+    }
+    basic.pause(1000)
+    if (game.score() == 4) {
+        game.gameOver()
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(3)
+    if (game.isPaused()) {
+        game.resume()
+    } else {
+        game.pause()
+    }
+})
+let Shoot: game.LedSprite = null
+let sprite: game.LedSprite = null
+let Player: game.LedSprite = null
+let Enemy: game.LedSprite = null
+game.setLife(4)
+game.setScore(0)
+joystickbit.initJoystickBit()
+Enemy = game.createSprite(0, 0)
+Player = game.createSprite(4, 4)
+radio.setGroup(1)
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) <= 200) {
+        radio.sendNumber(1)
+        Player.change(LedSpriteProperty.X, 1)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) >= 800) {
+        radio.sendNumber(2)
+        Player.change(LedSpriteProperty.X, -1)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) <= 200) {
+        radio.sendNumber(5)
+        Player.change(LedSpriteProperty.Y, 1)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) >= 800) {
+        radio.sendNumber(6)
+        Player.change(LedSpriteProperty.Y, -1)
+    }
+    basic.pause(100)
+})
